1065110c8966a65d02a6dfda47d2a5380f2d7fbd
ryanbegell <ryanbegell@outlook.com>
2024-03-06 17:47:15 -0500

    dunny data and sample images for use during development

---
+++ src/constants/board-data.ts
@@ -0,0 +1,26 @@
+// boardData.js
+export const boardData = [
+  {
+    id: 'column-1',
+    title: 'To Do',
+    cards: [
+      { id: 'card-1', title: 'Task 1', description: 'Description for Task 1' },
+      { id: 'card-2', title: 'Task 2', description: 'Description for Task 2' },
+    ],
+  },
+  {
+    id: 'column-2',
+    title: 'In Progress',
+    cards: [
+      { id: 'card-3', title: 'Task 3', description: 'Description for Task 3' },
+    ],
+  },
+  {
+    id: 'column-3',
+    title: 'Done',
+    cards: [
+      { id: 'card-4', title: 'Task 4', description: 'Description for Task 4' },
+      { id: 'card-5', title: 'Task 5', description: 'Description for Task 5' },
+    ],
+  },
+];
