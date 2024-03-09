2c21ddf496532f9fea18d3147649c2143cd5812d
ryanbegell <ryanbegell@outlook.com>
2024-03-09 16:59:47 -0500

    Landing page, favicon swap, hello-pangea/dnd dependency added

---
+++ src/components/landing/landing.tsx
@@ -0,0 +1,60 @@
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+interface LandingProps {
+  className?: string;
+}
+
+export default function Landing() {
+  return (
+    <div className="flex flex-col flex-grow">
+      <main className="flex-1 flex items-center justify-center">
+        <section className="w-full ">
+          <div className="container flex flex-col items-center justify-center space-y-6 px-4 md:px-6">
+            <div className="space-y-6 text-center">
+              <div className="flex justify-center">
+                <div className="bg-muted py-1 px-2 rounded-md inline-flex items-center">
+                  <span className="text-sm">🛠️</span>
+                  <span
+                    role="none"
+                    className="bg-border dark:bg-muted-foreground w-[1px] mx-2 h-4 inline-block"
+                  ></span>
+                  <a
+                    href="https://github.com/RyanBeGell/agile-board-front-end-react"
+                    className="text-xs font-semibold flex items-center"
+                  >
+                    {' '}
+                    {/* Changed from <p> to <div> */}
+                    Built with React, Springboot, TailwindCSS and more
+                    <span className="inline-flex items-center ml-1">
+                      <ArrowRight className="h-4 w-4" />
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <h1 className="text-6xl font-bold tracking-tighter sm:text-6xl">
+                <span className="gradient-text">Agilify </span> your project
+                workflow
+              </h1>
+              <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                The platform for rapid progress. Designed for individuals and
+                teams seeking efficiency without the clutter, Agilify
+                streamlines your projects, tasks, and deadlines into a clear,
+                manageable workflow.
+              </p>
+            </div>
+            <div className="mx-auto w-full max-w-sm space-y-2">
+              <div className="flex justify-center space-x-4">
+                <Button size="lg" className="btn-glow">
+                  Try Agilify now
+                </Button>
+                <Button variant={'outline'} size="lg">
+                  Learn more
+                </Button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
